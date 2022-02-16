130c89d3b45d8d879e3801e009f10ced96aa66f4
Lucas-Gallani <lucasgallani2022@gmail.com>
2022-02-16 10:41:05 -0300

    Exercicio_2

---
+++ Exercicios/Exercicio_2/assets/js/Ex002.js
@@ -0,0 +1,20 @@
+function Verificar() {
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('textano')
+   var res = document.getElementById('res')
+   if (fano.value.length == 0 || Number(fano.value) > ano) {
+      window.alert('[ERRO] Verfique os dados e tente novamente!')
+   } else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var genero = ''
+      if (fsex[1].checked) {
+         genero = 'Mulher'
+      } else if (fsex[0].checked) {
+         genero = 'Homem'
+      }
+      res.style.textAlign = 'cennter'
+      res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+   }
+}
